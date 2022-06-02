beb65d5baa4fc1ec544da5499798dedb7d9c9afb
data-TUNSW <106497056+data-TUNSW@users.noreply.github.com>
2022-06-02 14:20:59 +1000

    Update
    Initial work to make variable geographies

--- src/constants.py
+++ src/constants.py
@@ -9,9 +9,12 @@ base_data_dir = (Path(project_dir).parents[1].absolute()).as_posix() + '/ABS/Geo
 raw_data_dir = base_data_dir + '/raw/'
 processed_data_dir = base_data_dir + '/processed/'
 
+geo_codes = ['POA', 'LGA']
+geo_names = {'POA': 'Postcode', 'LGA': 'LGA'}
+
 raw_data_file_CA = raw_data_dir + 'CA_POSTCODE_2018_LGA_2018.xlsx'
 raw_data_file_lprw = raw_data_dir + 'POA_LGA_Rented_2016.xlsx'
-geography_transforms = {'LGA': {'2016-2021': raw_data_dir + 'CG_LGA_2016_LGA_2021.csv'},
+geography_updates = {'LGA': {'2016-2021': raw_data_dir + 'CG_LGA_2016_LGA_2021.csv'},
     'POA': {'2016-2021': raw_data_dir + 'CG_POA_2016_POA_2021.csv'}}
 
 read_options_CA = {'sheet_name': "Table 3", 'skiprows': [0, 1, 2, 3, 4, 6], 'skipfooter': 6, 'usecols': 'A,C:E'}
